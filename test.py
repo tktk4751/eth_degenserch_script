c657f8cc59cebb4b2ca200b46e67afa519647e7f
tktk4751 <105689723+tktk4751@users.noreply.github.com>
2023-12-20 15:04:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 
 # APIキーとウォレットアドレスとコントラクトアドレス（Uniswap V2）を設定する
-api_key = "ZI456ZQ8K6K7RUCRPJAB8MJDI1UAHXHVCF"
+api_key = "YOUR_AIP_KEY"
 wallet_address = "0x2095df2d67b1d2eb71b5af6bdf09bec261cc5a89"
 contract_address = "0x7a250d5630b4cf539739df2c5dacb4c659f2488d"
 
